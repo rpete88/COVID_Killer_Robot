0da8667e2074a142f340e557eee68e8df0d9aae1
mertzr <mertzr@udel.edu>
2020-11-12 15:33:53 -0500

    Update racoon_detector.py

--- racoon_detector.py
+++ racoon_detector.py
@@ -1,7 +1,7 @@
 #tested with frimware 5-0.22
 import sensor,image,lcd
 import KPU as kpu
-
+print("program starting")
 lcd.init()
 sensor.reset()
 sensor.set_pixformat(sensor.RGB565)
@@ -9,11 +9,17 @@ sensor.set_framesize(sensor.QVGA)
 sensor.set_windowing((224, 224))
 sensor.set_vflip(1)
 sensor.run(1)
-classes = ["racoon"]
-task = kpu.load(0x200000) #change to "/sd/name_of_the_model_file.kmodel" if loading from SD card
+print("after sensor statements")
+classes = ["desk"]
+print("test1")
+task = kpu.load("/sd/desk.kmodel") #change to "/sd/name_of_the_model_file.kmodel" if loading from SD card
+print("test2")
 a = kpu.set_outputs(task, 0, 7,7,30)   #the actual shape needs to match the last layer shape of your model(before Reshape)
+print("test3")
 anchor = (0.57273, 0.677385, 1.87446, 2.06253, 3.33843, 5.47434, 7.88282, 3.52778, 9.77052, 9.16828)
+print("test4")
 a = kpu.init_yolo2(task, 0.3, 0.3, 5, anchor) #tweak the second parameter if you're getting too many false positives
+print("entering while loop")
 while(True):
     img = sensor.snapshot().rotation_corr(z_rotation=90.0)
     a = img.pix_to_ai()
