448680d24df8561cb2a88c8aef3774abe2e92d29
MarkBattle10 <60949764+MarkBattle10@users.noreply.github.com>
2021-03-16 18:41:20 -0400

    Fixed the hcsr04 class to accept gpio pin as input

--- Spring2021/ultra.py
+++ Spring2021/ultra.py
@@ -11,7 +11,7 @@ class HCSR04:
     The timeouts received listening to echo pin are converted to OSError('Out of range')
     """
     # echo_timeout_us is based in chip range limit (400cm)
-    def __init__(self, trigpin, echopin, echo_timeout_us=500*2*30):
+    def __init__(self, trigpin, echopin, FMGPIOTrig, FMGPIOEcho, GPIOTrig, GPIOEcho, echo_timeout_us=500*2*30):
         """
         trigger_pin: Output pin to send pulses
         echo_pin: Readonly pin to measure the distance. The pin should be protected with 1k resistor
@@ -20,21 +20,21 @@ class HCSR04:
         """
 
 
-        fm.register(trigpin, fm.fpioa.GPIO0, force=True) # 15 - trig_dispensor
-        fm.register(echopin, fm.fpioa.GPIO1, force=True) # 17 - echo_dsipenser
+        fm.register(trigpin, FMGPIOTrig, force=True) # 15 - trig_dispensor
+        fm.register(echopin, FMGPIOEcho, force=True) # 17 - echo_dsipenser
 
 
         print("trigger gpio ", trigpin)
         print("echo gpio ", echopin)
         self.echo_timeout_us = echo_timeout_us
         # Init trigger pin (out)
-        self.trigger = GPIO(GPIO.GPIO0, GPIO.OUT)
+        self.trigger = GPIO(GPIOTrig, GPIO.OUT)
         self.trigger.value(0)
 
         # Init echo pin (in)
-        self.echo = GPIO(GPIO.GPIO1, GPIO.IN, GPIO.PULL_NONE)
+        self.echo = GPIO(GPIOEcho, GPIO.IN, GPIO.PULL_NONE)
 
-    def _send_pulse_and_wait(self):
+    def send_pulse_and_wait(self):
         """
         Send the pulse to trigger and listen on echo pin.
         We use the method `machine.time_pulse_us()` to get the microseconds until the echo is received.
@@ -75,7 +75,7 @@ class HCSR04:
         """
         Get the distance in milimeters without floating point operations.
         """
-        pulse_time = self._send_pulse_and_wait()
+        pulse_time = self.send_pulse_and_wait()
 
         # To calculate the distance we get the pulse_time and divide it by 2
         # (the pulse walk the distance twice) and by 29.1 becasue
@@ -90,7 +90,7 @@ class HCSR04:
         Get the distance in centimeters with floating point operations.
         It returns a float
         """
-        pulse_time = self._send_pulse_and_wait()
+        pulse_time = self.send_pulse_and_wait()
         print("pulse time is : ", pulse_time)
 
         # To calculate the distance we get the pulse_time and divide it by 2
@@ -105,13 +105,13 @@ class HCSR04:
         Get the distance in centimeters with floating point operations.
         It returns a float
         """
-        pulse_time = self._send_pulse_and_wait()
+        pulse_time = self.send_pulse_and_wait()
         print("pulse time is : ", pulse_time)
 
         # To calculate the distance we get the pulse_time and divide it by 2
         # (the pulse walk the distance twice) and by 29.1 becasue
         # the sound speed on air (343.2 m/s), that It's equivalent to
-        # 0.0135039 in/us that is 1cm each 29.1us
-        inches = (pulse_time / 2) / 74.05
+        # 0.0135039 in/us that is 1cm each 74.05us
+        inches = ((pulse_time / 2) / 74.05)
         print("inches: ", inches)
         return inches
